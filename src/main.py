5b0c102044d7fb48f34698c2d7aa43857493678b
Tom Lanphen <tomlanphen@gmail.com>
2021-05-27 16:38:53 +0200

    extra draaiveiligheid

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-import instructiemaker, wiel, pad, uarthandeler
+from src import instructiemaker, wiel, pad, uarthandeler, kompas
+from __future__ import print_function
+import time, sys, signal, atexit, math
+try:
+    from upm import pyupm_bmm150 as sensorObj
+except ImportError:
+    print('Error: Please install python-mraa python-upm module.\r\n' 
+          'See instruction here https://github.com/Seeed-Studio/pi_repo#mraa--upm-package-repository-for-raspberry-pi ')
 
 
 def setup_wielen():
@@ -18,7 +25,8 @@ def setup_wielen():
 def setup_pad():
     padarray = pad.Pad()
     #padarray.set_vector(0, 10)
-    padarray.set_vector(90, 10)
+    padarray.set_vector(90, 0)
+    padarray.set_vector(0, 10)
     #padarray.set_vector(-90, 10)
 
     return padarray
@@ -26,11 +34,30 @@ def setup_pad():
 
 if __name__ == '__main__':
     volgen = setup_pad()
+    extraDraai = pad.Pad()
     wielen = setup_wielen()
     uart = uarthandeler.Uarthandeler()
+    richting = kompas.Kompas()
 
     instructies = instructiemaker.Instructiemaker()
+
     for i in range(len(volgen.vectoren)):
+        beginRichting = richting.lees_richting()
         instructies.rijinstructies.wielinstructies = []
         instructies.maak_instructie(wielen, volgen.get_vector())
         uart.stuur_instructie(instructies)
+
+        # als er gedraaid moet zijn en de draai is niet volledig draai dan extra
+        while uart.draaicheck != 0:
+            huidigeRichting = richting.lees_richting()
+            verwachteRichting = richting.lees_richting() + uart.draaicheck
+            if verwachteRichting > 360:
+                verwachteRichting -= 360
+            berekendeDraai = uart.draaicheck - (verwachteRichting - huidigeRichting)
+            if -5 < berekendeDraai < 5:
+                extraDraai.set_vector(int(berekendeDraai), 0)
+                instructies.rijinstructies.wielinstructies = []
+                instructies.maak_instructie(wielen, extraDraai.get_vector())
+                uart.stuur_instructie(instructies)
+            else:
+                uart.draaicheck = 0
--- src/uarthandeler.py
+++ src/uarthandeler.py
@@ -5,7 +5,7 @@ class Uarthandeler:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(17, GPIO.IN)
-
+        self.draaicheck = 0
         self.ser = serial.Serial('/dev/ttyS0', baudrate=9600,
                             parity=serial.PARITY_NONE,
                             stopbits=serial.STOPBITS_ONE,
@@ -14,11 +14,15 @@ class Uarthandeler:
 
     def stuur_instructie(self, instructie):
         for rijinstructie in instructie.rijinstructies.wielinstructies:
-            print("niew")
+            # wacht tot de vorige actie klaar is
             while GPIO.input(17):
                 niks = False
 
+            # stuur de vier instructies naar de vier wielen en geef ze het start signaal
             for wielinstructie in rijinstructie:
+                if wielinstructie.draai != 0:
+                    # een variabele die gebruikt kan worden om te kijken of de juiste hoek gedraaid is
+                    self.draaicheck = wielinstructie.draai
                 self.ser.write(wielinstructie.instructie)
 
             self.ser.write(bytes("sssssss", 'utf-8'))
